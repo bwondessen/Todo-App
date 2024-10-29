670eb50820aeb4e55e5c195287020a534cab84a8
bwondessen <bwondessen23@gmail.com>
2024-10-29 17:26:05 -0500

    Add taskObj objects to taskData array

--- script.js
+++ script.js
@@ -39,4 +39,8 @@ taskForm.addEventListener("submit", (e) => {
         date: dateInput.value,
         description: descriptionInput.value,
     };
+
+    if (dataArrIndex === -1) {
+        taskData.unshift(taskObj);
+    }
 });
