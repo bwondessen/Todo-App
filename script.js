ca2047ffb908b9c22ff0306be2b56804325e9b7f
bwondessen <bwondessen23@gmail.com>
2024-10-29 18:44:54 -0500

    Clear input fields after creating tasks

--- script.js
+++ script.js
@@ -13,6 +13,15 @@ const descriptionInput = document.getElementById("description-input");
 const taskData = [];
 let currentTask = {};
 
+const reset = () => {
+    titleInput.value = "";
+    dateInput.value = "";
+    descriptionInput.value = "";
+
+    taskForm.classList.toggle("hidden");
+    currentTask = {};
+}
+
 openTaskFormBtn.addEventListener("click", () =>
     taskForm.classList.toggle("hidden")
 );
@@ -44,8 +53,9 @@ taskForm.addEventListener("submit", (e) => {
         taskData.unshift(taskObj);
     }
 
-    taskData.forEach(({ id, title, date, description }) => {
-        tasksContainer.innerHTML += `
+    taskData.forEach(
+        ({ id, title, date, description }) => {
+            tasksContainer.innerHTML += `
         <div class="task" id="${id}">
           <p><strong>Title:</strong> ${title}</p>
           <p><strong>Date:</strong> ${date}</p>
@@ -54,8 +64,8 @@ taskForm.addEventListener("submit", (e) => {
           <button type="button" class="btn">Delete</button>
         </div>
       `
-    }
+        }
     );
+
     taskForm.classList.toggle("hidden");
 });
-
