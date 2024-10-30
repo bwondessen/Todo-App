cbf122a8d89c55643ac045f65e2ae1922ff25d7c
bwondessen <bwondessen23@gmail.com>
2024-10-29 19:09:37 -0500

    Enhance code readability and maintainability

--- script.js
+++ script.js
@@ -13,6 +13,36 @@ const descriptionInput = document.getElementById("description-input");
 const taskData = [];
 let currentTask = {};
 
+const addOrUpdateTask = () => {
+    const dataArrIndex = taskData.findIndex((item) => item.id === currentTask.id);
+    const taskObj = {
+        id: `${titleInput.value.toLowerCase().split(" ").join("-")}-${Date.now()}`,
+        title: titleInput.value,
+        date: dateInput.value,
+        description: descriptionInput.value,
+    };
+
+    if (dataArrIndex === -1) {
+        taskData.unshift(taskObj);
+    }
+};
+
+const updateTaskContainer = () => {
+    taskData.forEach(
+        ({ id, title, date, description }) => {
+            tasksContainer.innerHTML += `
+        <div class="task" id="${id}">
+          <p><strong>Title:</strong> ${title}</p>
+          <p><strong>Date:</strong> ${date}</p>
+          <p><strong>Description:</strong> ${description}</p>
+          <button type="button" class="btn">Edit</button>
+          <button type="button" class="btn">Delete</button>
+        </div>
+      `
+        }
+    );
+};
+
 const reset = () => {
     titleInput.value = "";
     dateInput.value = "";
@@ -43,32 +73,6 @@ discardBtn.addEventListener("click", () => {
 
 taskForm.addEventListener("submit", (e) => {
     e.preventDefault();
-
-    const dataArrIndex = taskData.findIndex((item) => item.id === currentTask.id);
-    const taskObj = {
-        id: `${titleInput.value.toLowerCase().split(" ").join("-")}-${Date.now()}`,
-        title: titleInput.value,
-        date: dateInput.value,
-        description: descriptionInput.value,
-    };
-
-    if (dataArrIndex === -1) {
-        taskData.unshift(taskObj);
-    }
-
-    taskData.forEach(
-        ({ id, title, date, description }) => {
-            tasksContainer.innerHTML += `
-        <div class="task" id="${id}">
-          <p><strong>Title:</strong> ${title}</p>
-          <p><strong>Date:</strong> ${date}</p>
-          <p><strong>Description:</strong> ${description}</p>
-          <button type="button" class="btn">Edit</button>
-          <button type="button" class="btn">Delete</button>
-        </div>
-      `
-        }
-    );
 
     reset()
 });
