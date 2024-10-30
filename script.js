bce5c579cc4b84b08aa226e8be2435ed61ddec10
bwondessen <bwondessen23@gmail.com>
2024-10-29 19:20:21 -0500

    Fix task duplication bug

--- script.js
+++ script.js
@@ -31,7 +31,7 @@ const addOrUpdateTask = () => {
 };
 
 const updateTaskContainer = () => {
-
+    tasksContainer.innerHTML = "";
     taskData.forEach(
         ({ id, title, date, description }) => {
             tasksContainer.innerHTML += `
